abd6b529123b4208d8a7c74e19aa756faab1a97e
Your Name <you@example.com>
2022-06-11 20:01:57 +0300

    Fix models.py syntax error

--- app/models.py
+++ app/models.py
@@ -12,9 +12,9 @@ from app.database import Base
 class User(Base):
     __tablename__ = consts.USERS_TABLE_NAME
 
-    username = Column(String[consts.MAX_USERNAME_LENGTH], primary_key=True)
-    password_hash = Column(String[consts.PASSWORD_HASH_MAX_LENGTH])
-    status = Column(String[consts.USER_STATUS_MAX_LENGTH])
+    username = Column(String(consts.MAX_USERNAME_LENGTH), primary_key=True)
+    password_hash = Column(String(consts.PASSWORD_HASH_MAX_LENGTH))
+    status = Column(String(consts.USER_STATUS_MAX_LENGTH))
 
     def set_password(self, password: str):
         """Encrypt the password and save it as a property."""
