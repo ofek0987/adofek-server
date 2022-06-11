7d0bb33b73d2aa26dbe8785d29aeb5d2e212bc05
Your Name <you@example.com>
2022-06-11 16:18:30 +0300

    Declare TextMessage

---
+++ app/messages_managment/messages/text_message.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+from app.messages_managment.messages.base_message import BaseMessage
+
+
+@dataclass
+class TextMessage(BaseMessage):
+    text: str
+
+    def to_json(self) -> str:
+        pass
