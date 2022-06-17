9d0c6e8d6320eacea18cf44d5174ed04bf02e949
Your Name <you@example.com>
2022-06-17 13:30:23 +0300

    Implement image message

--- app/messages_managment/messages/image_message.py
+++ app/messages_managment/messages/image_message.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
+from app.enums.message_type import MessageType
 from app.messages_managment.messages.file_message import FileMessage
 
 
 class ImageMessage(FileMessage):
-    ...
+
+    def type(self) -> MessageType:
+        return MessageType.IMAGE
