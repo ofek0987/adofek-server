7140d754eb0c89b51490018999c444822ce0f962
Your Name <you@example.com>
2022-06-17 13:28:03 +0300

    Implement text message

--- app/messages_managment/messages/text_message.py
+++ app/messages_managment/messages/text_message.py
@@ -1,19 +1,17 @@
 from __future__ import annotations
 
-import json
 from dataclasses import dataclass
 
-from app import consts
 from app.enums.message_type import MessageType
 from app.messages_managment.messages.base_message import BaseMessage
 
 
 @dataclass
 class TextMessage(BaseMessage):
-    """Ordinary text message sent in the chat."""
+    """Ordinary text message."""
+
     text: str
 
-    def to_json(self) -> str:
-        future_json = self._get_vars_dict_for_json()
-        future_json[consts.MESSAGE_JSON_TYPE_FIELD] = MessageType.TEXT.value
-        return json.dumps(future_json)
+    @property
+    def type(self) -> MessageType:
+        return MessageType.TEXT
