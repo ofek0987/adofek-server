f4eea54606791f3340a3550b7eb3a1e521b7ed11
Your Name <you@example.com>
2022-06-17 13:24:55 +0300

    Extend in BaseMessage in order to avoid code duplication

--- app/messages_managment/messages/base_message.py
+++ app/messages_managment/messages/base_message.py
@@ -1,9 +1,16 @@
 from __future__ import annotations
 
+import json
 from abc import ABCMeta
 from abc import abstractmethod
 from dataclasses import dataclass
 from datetime import datetime
+from typing import TYPE_CHECKING
+
+from app import consts
+
+if TYPE_CHECKING:
+    from app.enums.message_type import MessageType
 
 
 @dataclass  # type: ignore
@@ -14,14 +21,21 @@ class BaseMessage(metaclass=ABCMeta):
     sent_timestamp: datetime
 
     @abstractmethod
+    @property
+    def type(self) -> MessageType:
+        """Message type in enum representation."""
+        ...
+
     def to_json(self) -> str:
         """Represent a message in json form.
         Used for sending/receiving messages."""
-        ...
+        dict_json = self._get_vars_dict_for_json()
+        dict_json[consts.MESSAGE_JSON_TYPE_FIELD] = self.type.value
+        return json.dumps(dict_json)
 
     def _get_vars_dict_for_json(self) -> dict:
         """Generate stringified dict from the class's properties."""
         return {
-            str(key): str(value)
+            key: str(value)
             for key, value in vars(self).items()
         }
