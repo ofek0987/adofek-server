3e89dd2b2dc8d65637d27ae4ecc020a79844e8f1
Your Name <you@example.com>
2022-06-17 16:01:17 +0300

    Implement messages pool

--- app/messages_managment/messages_pool.py
+++ app/messages_managment/messages_pool.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import bisect
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -7,8 +8,25 @@ if TYPE_CHECKING:
 
 
 class MessagesPool:
-    def push(self, message: BaseMessage):
-        ...
+    """Stores all the received messages in memory."""
 
-    def pull(self, message_id: str) -> BaseMessage:
-        ...
+    def __init__(self):
+        self._messages_pool: dict[str, list[BaseMessage]] = {}
+
+    def push_message(self, message: BaseMessage):
+        """Add a message to the message pool."""
+        if message.to_user in self._messages_pool.keys():
+            bisect.insort(self._messages_pool[message.to_user], message)
+        else:
+            self._messages_pool[message.to_user] = [message]
+
+    def pull_messages_assigned_to_user(self, user: str) -> list[BaseMessage]:
+        """
+        Get all the messages assigned to a user
+        and remove them from the messages pool.
+        :param user: The username of the user
+                     that the message should be assigned to.
+        :return: List of all the relevant messages
+                 sorted by their sent timestamp.
+        """
+        return self._messages_pool.pop(user, [])
