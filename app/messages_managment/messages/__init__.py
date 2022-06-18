71673bf6c64f7d687c5e403233213688fe50f378
Your Name <you@example.com>
2022-06-18 20:48:14 +0300

    Messages validation functionality

--- app/messages_managment/messages/__init__.py
+++ app/messages_managment/messages/__init__.py
@@ -6,6 +6,7 @@ from .message_failed_error_message import MessageFailedErrorMessage
 from .message_sent_message import MessageSentMessage
 from app import consts
 from app.enums.message_type import MessageType
+from app.exceptions.validation_error import ValidationError
 from app.messages_managment.messages.base_message import BaseMessage
 from app.messages_managment.messages.error_message import ErrorMessage
 from app.messages_managment.messages.file_message import FileMessage
@@ -16,9 +17,9 @@ from app.messages_managment.messages.text_message import TextMessage
 def resolve_json_to_message(json_data: Json) -> BaseMessage:
     """
     Create message object based on its json representation.
-    Assumes the json data is valid.
     :param json_data: The json data to resolve.
     :return: Message object
+    :raises ValidationError: If the given json is invalid.
     """
     message_type_to_message_class_resolver: dict[
         MessageType,
@@ -31,6 +32,9 @@ def resolve_json_to_message(json_data: Json) -> BaseMessage:
         MessageType.MESSAGE_FAIL: MessageFailedErrorMessage,
         MessageType.MESSAGE_SENT: MessageSentMessage,
     }
-    message_type = MessageType(json_data[consts.MESSAGE_JSON_TYPE_FIELD])
-    return message_type_to_message_class_resolver[message_type] \
-        .from_dict(json_data)
+    try:
+        message_type = MessageType(json_data[consts.MESSAGE_JSON_TYPE_FIELD])
+        return message_type_to_message_class_resolver[message_type] \
+            .from_dict(json_data)
+    except (KeyError, ValueError):
+        raise ValidationError(f'{json_data} is not a valid message.')
